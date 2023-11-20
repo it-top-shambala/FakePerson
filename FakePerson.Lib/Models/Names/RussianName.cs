5e0989fac3e9f84fd0a3ada690c051074ce45f14
Starinin Andrey <starininandrey@gmail.com>
2023-11-20 20:15:19 +0300

    Добавление "русского имени"

---
+++ FakePerson.Lib/Models/Names/RussianName.cs
@@ -0,0 +1,7 @@
+﻿namespace FakePerson.Lib.Models.Names;
+
+public record RussianName : AbstractName
+{
+    public required string LastName { get; init; }
+    public string? Patronymic { get; init; }
+}
